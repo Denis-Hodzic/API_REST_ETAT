1256c2c30133ff423ee8291faa549906c27c9d30
Alexandre Ostier <alexandre.ostier@etu.univ-smb.fr>
2026-02-03 17:12:27 +0100

    Ajout de test

---
+++ API_REST_ETATTests/Controllers/SeriesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using API_REST_ETAT.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_REST_ETAT.Controllers.Tests
+{
+    [TestClass()]
+    public class SeriesControllerTests
+    {
+        [TestMethod()]
+        public void GetSeriesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
